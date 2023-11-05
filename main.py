7161e566e7e7051a62b697d776147259e6c32740
Isaiah Jones <isaiahlkjones00@gmail.com>
2023-11-05 15:51:27 -0500

    stage 1

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.logging import logger
 
-logger.info("Running Text Summarizer")
+STAGE_NAME = "Data Ingestion Stage 01"
+try:
+    logger.info(f"Started {STAGE_NAME}")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f"{STAGE_NAME} completed successfully")
+except Exception as e:
+    logger.exception(e)
+    raise e
+

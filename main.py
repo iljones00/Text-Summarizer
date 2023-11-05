68f8bd16433b5c4471f84734b0fccc42d99f55d5
Isaiah Jones <isaiahlkjones00@gmail.com>
2023-11-05 16:47:18 -0500

    stage 2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarizer.logging import logger
 
 STAGE_NAME = "Data Ingestion Stage 01"
@@ -11,3 +12,14 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+STAGE_NAME = "Data Validation Stage 02"
+try:
+    logger.info(f"Started {STAGE_NAME}")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f"{STAGE_NAME} completed successfully")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = 
--- src/textSummarizer/pipeline/stage_01_data_ingestion.py
+++ src/textSummarizer/pipeline/stage_01_data_ingestion.py
@@ -2,8 +2,6 @@ from textSummarizer.config.configuration import ConfigurationManager
 from textSummarizer.components.data_ingestion import DataIngestion
 from textSummarizer.logging import logger
 
-
-
 class DataIngestionTrainingPipeline:
     def __init__(self):
         pass

33f6678299607ac4dce22f2366a53355a0c0e697
Isaiah Jones <isaiahlkjones00@gmail.com>
2023-11-05 17:21:58 -0500

    stage 3

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from textSummarizer.logging import logger
 
 STAGE_NAME = "Data Ingestion Stage 01"
@@ -22,4 +23,12 @@ except Exception as e:
     logger.exception(e)
     raise e
 
-STAGE_NAME = 
+STAGE_NAME = "Data Transformation Stage 03"
+try:
+    logger.info(f"Started {STAGE_NAME}")
+    data_validation = DataTransformationTrainingPipeline()
+    data_validation.main()
+    logger.info(f"{STAGE_NAME} completed successfully")
+except Exception as e:
+    logger.exception(e)
+    raise e
